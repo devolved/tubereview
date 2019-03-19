97d96d544204ffabbc5aec8ebc3947082d653720
devolved <devolved@gmail.com>
2019-03-19 00:39:49 +0000

    Spit out latest on homepage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var url = 'mongodb://localhost:27017/tube';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Tube Review' });
+
+  // latest reviews
+  MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+    if (err) { return console.log('Unable to connect to MongoDB'); } 
+
+    //get result from db     
+    client.db('tube').collection('channel-reviews').find({}).limit(10).sort({ $natural: -1 }).toArray(function(err, result) {
+      if (err) throw err;
+
+      console.log(result);
+      client.close();
+
+
+
+    //output page
+    res.render('index', { title: 'Tube Review', result });
+
+    });
+  });
+
+
+
 });
+
+
 
 module.exports = router;
